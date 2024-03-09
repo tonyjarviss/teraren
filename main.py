23f2e41237e000f5ccce681ef0a004ac64b0bc12
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-03-09 11:12:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def start(m: UpdateNewMessage):
 Hello! I am a bot to download videos from terabox.
 Send me the terabox link and I will start downloading it.
 """
- InlineKeyboardButton(text="Join Channel", callback_url="https://t.me/mavimods2") 
+ reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/mavimods2")]]), reply_to_message_id=message.id) 
 
     check_if = await is_user_on_chat(bot, "@mavimods2", m.peer_id)
     if not check_if:
