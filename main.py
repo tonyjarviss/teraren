384057e35ae9a3f1e75208e33441740ceac46672
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-03-09 22:17:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ db = redis.Redis(
 )
 
 
-@bot.on_message(filters.command(["start"]))
+@bot.on(events.NewMessage(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
 	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/mavimods2")]]), reply_to_message_id=message.id)
